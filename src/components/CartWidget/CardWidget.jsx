ecc25e86732fc6538987b4edcc515c09aa5ef943
josediazangarita <josediazangarita@gmail.com>
2023-10-30 00:32:20 -0300

    Inicialización de estilos de mi proyecto

---
+++ src/components/CartWidget/CardWidget.jsx
@@ -0,0 +1,6 @@
+
+export const CardWidget = () => {
+  return (
+    <div>CardWidget</div>
+  )
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import { CardWidget } from "../CartWidget/CardWidget"
+
+export const NavBar = () => {
+  return (
+    <div>
+        NavBar
+        <CardWidget />
+    </div>
+  )
+}
